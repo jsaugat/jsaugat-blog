bb1bdc29deabf94c7e78b8c3c8912fab85c190c5
Saugat Joshi <jsaugatt02@gmail.com>
2024-09-19 12:19:55 +0545

    .

--- app/page.tsx
+++ app/page.tsx
@@ -16,14 +16,11 @@ export default function Home() {
           Saugat&apos;s Blog
         </div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
+
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            blog under construction
+          </code>
+
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
