1c3042215cb5ff554135dc1c67d5557d2a996be1
Saugat Joshi <jsaugatt02@gmail.com>
2024-09-19 11:24:06 +0545

    apos

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           priority
         /> */}
         <div className="text-4xl font-semibold">
-          Saugat's Blog
+          Saugat&apos;s Blog
         </div>
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
